5013b3448ec6f259641fc0618388dd1075bbad3a
Adam Ozeri <80855756+adamozeri@users.noreply.github.com>
2022-11-27 21:13:31 +0200

    obstacle updates

--- Android_HW1/app/src/main/java/com/example/android_hw1/GameActivity.java
+++ Android_HW1/app/src/main/java/com/example/android_hw1/GameActivity.java
@@ -3,8 +3,11 @@ package com.example.android_hw1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.LinearLayout;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.imageview.ShapeableImageView;
@@ -14,7 +17,7 @@ import java.util.TimerTask;
 
 public class GameActivity extends AppCompatActivity {
     private Timer timer;
-    private final int DELAY = 1000;
+    private final int DELAY = 750;
 
     private ExtendedFloatingActionButton game_FAB_leftArrow;
     private ExtendedFloatingActionButton game_FAB_rightArrow;
@@ -22,8 +25,9 @@ public class GameActivity extends AppCompatActivity {
     private LinearLayout game_LL_player;
     private ShapeableImageView[] game_IMG_hearts;
 
-    private GameManger gameManger;
+    private ShapeableImageView game_IMG_background;
 
+    private GameManger gameManger;
 
 
     @Override
@@ -31,12 +35,18 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         findViews();
+        Glide
+                .with(this)
+                .load("https://img.freepik.com/free-vector/chalkboard-texture_1048-1125.jpg?w=826&t=st=1669559891~exp=1669560491~hmac=fa28ced811066e461dbd16b6075c6c8178fd0077b64ef1f86f8aac74e1bcfa93")
+                .into(game_IMG_background);
         this.gameManger = new GameManger(game_LL_obstacleCol, new Player()
-                .setCurrentPos(game_LL_player.getChildCount()/2+1)
+                .setCurrentPos(game_LL_player.getChildCount() / 2)
                 .setLife(game_IMG_hearts.length)
-                .setMaxIndex(game_LL_player.getChildCount()-1));
-        gameManger.initObstacles();
+                .setMaxIndex(game_LL_player.getChildCount() - 1));
+
+        initGameUI();
         refreshUI();
+        startTimer();
 
         game_FAB_leftArrow.setOnClickListener(view -> {
             clicked("left");
@@ -60,9 +70,9 @@ public class GameActivity extends AppCompatActivity {
         this.game_IMG_hearts = new ShapeableImageView[]{
                 findViewById(R.id.game_IMG_heart1),
                 findViewById(R.id.game_IMG_heart2),
-                findViewById(R.id.game_IMG_heart3),
-                findViewById(R.id.game_IMG_heart4)
+                findViewById(R.id.game_IMG_heart3)
         };
+        this.game_IMG_background = findViewById(R.id.game_IMG_background);
     }
 
 
@@ -71,22 +81,65 @@ public class GameActivity extends AppCompatActivity {
         refreshUI();
     }
 
-    private void refreshUI() {
-        if(gameManger.isEnded()){
+    private void initGameUI() {
+        initObstacles();
+        playerVisibility();
+    }
 
+    /**
+     * Sets all obstacles to invisible (we use it at the beginning of the game)
+     **/
+    private void initObstacles() {
+        for (int i = 0; i < game_LL_obstacleCol.length; i++) {
+            for (int j = 0; j < game_LL_obstacleCol[i].getChildCount(); j++) {
+                game_LL_obstacleCol[i].getChildAt(j).setVisibility(View.INVISIBLE);
+            }
         }
     }
 
+    /**
+     * sets player visibility in the current position
+     **/
+    private void playerVisibility() {
+        for (int i = 0; i < game_LL_player.getChildCount(); i++) {
+            if (i != gameManger.getPlayer().getCurrentPos())
+                game_LL_player.getChildAt(i).setVisibility(View.INVISIBLE);
+            else
+                game_LL_player.getChildAt(i).setVisibility(View.VISIBLE);
+        }
+    }
+
+    /**
+     * moves the obstacles on the screen with random spawns
+     **/
+    public void updateObstacles() {
+        int numRows = game_LL_obstacleCol[0].getChildCount() - 1;
+        for (int i = 0; i < game_LL_obstacleCol.length; i++) {
+            if (game_LL_obstacleCol[i].getChildAt(numRows).getVisibility() == View.VISIBLE) // checks the last obstacle
+                game_LL_obstacleCol[i].getChildAt(numRows).setVisibility(View.INVISIBLE);
+            for (int j = numRows; j > 0; j--) {
+                if (game_LL_obstacleCol[i].getChildAt(j - 1).getVisibility() == View.VISIBLE) { // Moves the obstacle one spot down
+                    game_LL_obstacleCol[i].getChildAt(j - 1).setVisibility(View.INVISIBLE);
+                    game_LL_obstacleCol[i].getChildAt(j).setVisibility(View.VISIBLE);
+                }
+            }
+        }
+        game_LL_obstacleCol[gameManger.randomSpawn()].getChildAt(0).setVisibility(View.VISIBLE); // setting a new obstacle in the first row
+        gameManger.hit();
+    }
 
 
+    private void refreshUI() {
+        playerVisibility();
+    }
 
     private void startTimer() {
         timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                runOnUiThread(()->{
-                    //TODO: my func
+                runOnUiThread(() -> {
+                    updateObstacles();
                 });
             }
         }, DELAY, DELAY);
--- Android_HW1/app/src/main/java/com/example/android_hw1/GameManger.java
+++ Android_HW1/app/src/main/java/com/example/android_hw1/GameManger.java
@@ -26,36 +26,63 @@ public class GameManger {
             player.setCurrentPos((player.getCurrentPos() + 1));
     }
 
+//    public void initGameUI() {
+//        initObstacles();
+//        initPlayer();
+//    }
+//
+//    /**
+//     * Sets all obstacles to invisible (we use it at the beginning of the game)
+//     **/
+//    public void initObstacles() {
+//        for (int i = 0; i < obstacleCols.length; i++) {
+//            for (int j = 0; j < obstacleCols[i].getChildCount(); j++) {
+//                obstacleCols[i].getChildAt(j).setVisibility(View.INVISIBLE);
+//            }
+//        }
+//    }
+//
+//    /**
+//     * Sets all player's image to invisible but the center one
+//     **/
+//    public void initPlayer() {
+//        for (int i = 0; i < playerLL.getChildCount(); i++) {
+//            if (i != player.getCurrentPos())
+//                playerLL.getChildAt(i).setVisibility(View.INVISIBLE);
+//            else
+//                playerLL.getChildAt(i).setVisibility(View.VISIBLE);
+//        }
+//    }
+
     /**
-     * Sets all obstacles to invisible (we use it at the beginning of the game)
+     * if the player collides with an obstacle lowers the player's life by 1
      **/
-    public void initObstacles() {
-        for (int i = 0; i < obstacleCols.length; i++) {
-            for (int j = 0; j < obstacleCols[i].getChildCount(); j++) {
-                obstacleCols[i].getChildAt(j).setVisibility(View.INVISIBLE);
-            }
-        }
+    public void hit() {
+        if (obstacleCols[player.getCurrentPos()].getChildAt((obstacleCols[player.getCurrentPos()].getChildCount() - 1)).getVisibility() == View.VISIBLE)
+            player.setLife(player.getLife() - 1);
     }
 
-    public boolean hit() {
-        return obstacleCols[player.getCurrentPos()].getChildAt((obstacleCols[player.getCurrentPos()].getChildCount() - 1)).getVisibility() == View.VISIBLE;
+//    public void updateObstacles() {
+//        int numRows = obstacleCols[0].getChildCount() - 1;
+//        for (int i = 0; i < obstacleCols.length; i++) {
+//            if (obstacleCols[i].getChildAt(numRows).getVisibility() == View.VISIBLE) // checks the last obstacle
+//                obstacleCols[i].getChildAt(numRows).setVisibility(View.INVISIBLE);
+//            for (int j = numRows; j > 0; j++) {
+//                if (obstacleCols[i].getChildAt(j - 1).getVisibility() == View.VISIBLE) { // Moves the obstacle one spot down
+//                    obstacleCols[i].getChildAt(j - 1).setVisibility(View.INVISIBLE);
+//                    obstacleCols[i].getChildAt(j).setVisibility(View.VISIBLE);
+//                }
+//            }
+//        }
+//        obstacleCols[randomSpawn()].getChildAt(0).setVisibility(View.VISIBLE); // setting a new obstacle in the first row
+//        hit();
+//    }
+
+    public int randomSpawn() {
+        return (int) (Math.random() * obstacleCols.length);
     }
 
-    public void updateObstacles() {
-        int numRows = obstacleCols[0].getChildCount() - 1;
-        for (int i = 0; i < obstacleCols.length; i++) {
-            if (obstacleCols[i].getChildAt(numRows).getVisibility() == View.VISIBLE) // checks the last obstacle
-                obstacleCols[i].getChildAt(numRows).setVisibility(View.INVISIBLE);
-            for (int j = numRows; j > 0; j++) {
-                if (obstacleCols[i].getChildAt(j - 1).getVisibility() == View.VISIBLE) { // Moves the obstacle one spot down
-                    obstacleCols[i].getChildAt(j - 1).setVisibility(View.INVISIBLE);
-                    obstacleCols[i].getChildAt(j).setVisibility(View.VISIBLE);
-                }
-            }
-        }
-    }
-
-    private int randomSpawn() {
-        return (int) (Math.random() * obstacleCols.length + 1);
+    public Player getPlayer() {
+        return player;
     }
 }
