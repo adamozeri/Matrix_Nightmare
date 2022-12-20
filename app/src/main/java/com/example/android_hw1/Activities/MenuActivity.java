86c4242c8c9226fc9febca9e801e21c368c8e001
Adam Ozeri <80855756+adamozeri@users.noreply.github.com>
2022-12-20 22:31:07 +0200

    created findViews() and openGameScreen(), also added view vars

--- app/src/main/java/com/example/android_hw1/Activities/MenuActivity.java
+++ app/src/main/java/com/example/android_hw1/Activities/MenuActivity.java
@@ -2,15 +2,66 @@ package com.example.android_hw1.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.CompoundButton;
 
 import com.example.android_hw1.R;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.switchmaterial.SwitchMaterial;
 
 public class MenuActivity extends AppCompatActivity {
+
+    // view
+    private MaterialButton menu_BTN_play;
+    private MaterialButton menu_BTN_topGrades;
+    private SwitchMaterial  menu_SW_sensor;
+    private SwitchMaterial  menu_SW_fast;
+
+    // mode
+    private boolean isFast;
+    private boolean isSensorMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        findViews();
+        menu_SW_fast.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean fast) {
+                isFast = fast;
+            }
+        });
+        menu_SW_sensor.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean sensor) {
+                isSensorMode = sensor;
+            }
+        });
+        menu_BTN_play.setOnClickListener(view -> {
+            openGameScreen();
+        });
+    }
+
+    private void findViews(){
+        menu_BTN_play = findViewById(R.id.menu_BTN_play);
+        menu_BTN_topGrades = findViewById(R.id.menu_BTN_topGrades);
+        menu_SW_sensor = findViewById(R.id.menu_SW_sensor);
+        menu_SW_fast = findViewById(R.id.menu_SW_fast);
+    }
+
+//    private void openGradesScreen(String status, int score) {
+//        Intent scoreIntent = new Intent(this, ScoreActivity.class);
+//        startActivity(scoreIntent);
+//        finish();
+//    }
+
+    private void openGameScreen() {
+        Intent gameIntent = new Intent(this, GameActivity.class);
+        gameIntent.putExtra(GameActivity.KEY_SENSOR, isSensorMode);
+        gameIntent.putExtra(GameActivity.KEY_SPEED, isFast);
+        startActivity(gameIntent);
+        finish();
     }
 }
