a33a8f907c69b02292ec79f87350869a71f6774c
Adam Ozeri <adamozeri10@gmail.com>
2022-12-21 15:11:03 +0200

    added list and map fragment

--- app/src/main/java/com/example/android_hw1/Activities/ScoreActivity.java
+++ app/src/main/java/com/example/android_hw1/Activities/ScoreActivity.java
@@ -4,13 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.android_hw1.Fragments.ListFragment;
+import com.example.android_hw1.Fragments.MapFragment;
 import com.example.android_hw1.R;
 
 public class ScoreActivity extends AppCompatActivity {
+
+    private ListFragment listFragment;
+    private MapFragment mapFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
+        listFragment = new ListFragment();
+        mapFragment = new MapFragment();
+
+        getSupportFragmentManager().beginTransaction().add(R.id.score_FRAME_list,listFragment).commit();
+        getSupportFragmentManager().beginTransaction().add(R.id.score_FRAME_map,mapFragment).commit();
+
     }
 }
