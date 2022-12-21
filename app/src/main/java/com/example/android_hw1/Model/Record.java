6fa430c107f14c098dfe5ee85e1bcef6ab3b8d6d
Adam Ozeri <adamozeri10@gmail.com>
2022-12-21 15:45:04 +0200

    added vars getters and setters

--- app/src/main/java/com/example/android_hw1/Model/Record.java
+++ app/src/main/java/com/example/android_hw1/Model/Record.java
@@ -1,4 +1,49 @@
 package com.example.android_hw1.Model;
 
 public class Record {
+
+    private int distance;
+    private double latitude;
+    private double longitude;
+    private String name;
+
+
+    public Record() {
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public Record setDistance(int distance) {
+        this.distance = distance;
+        return this;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public Record setLatitude(double latitude) {
+        this.latitude = latitude;
+        return this;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public Record setLongitude(double longitude) {
+        this.longitude = longitude;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Record setName(String name) {
+        this.name = name;
+        return this;
+    }
 }
