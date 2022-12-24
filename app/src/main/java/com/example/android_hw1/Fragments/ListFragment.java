6cfe45183e0c81a231b12f95ac69df5b27807461
Adam Ozeri <80855756+adamozeri@users.noreply.github.com>
2022-12-24 10:53:49 +0200

    added record list

--- app/src/main/java/com/example/android_hw1/Fragments/ListFragment.java
+++ app/src/main/java/com/example/android_hw1/Fragments/ListFragment.java
@@ -2,15 +2,17 @@ package com.example.android_hw1.Fragments;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.example.android_hw1.DataManager;
+import com.example.android_hw1.Model.Record;
 import com.example.android_hw1.R;
 
 public class ListFragment extends Fragment {
@@ -22,8 +24,15 @@ public class ListFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_list, container, false);
         findViews(view);
-
+        initListView();
         return view;
+    }
+
+    private void initListView() {
+        if(DataManager.getInstance().getTopRecords() != null){
+            ArrayAdapter<Record> adapter = new ArrayAdapter<Record>(getActivity(), android.R.layout.simple_expandable_list_item_1, DataManager.getInstance().getTopRecords());
+            list_LV.setAdapter(adapter);
+        }
     }
 
 
